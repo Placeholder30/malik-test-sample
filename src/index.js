0cee81c4c47b6cedc0dd50fff712bab1c4d72cfb
Malik <malikoabdulaziz@gmail.com>
2021-09-14 22:09:30 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const fetch = require("node-fetch");
 
 exports.addNumbers = (x, y) => x + y;
 
 /*
-  These are the functions that need to be tested.
-  Every function from here below needs to be tested.
+These are the functions that need to be tested.
+Every function from here below needs to be tested.
 */
 exports.subtractNumbers = (x, y) => x - y;
 
 exports.makeAjaxCall = async () => {
+  const fetch = require("node-fetch");
   const resp = await fetch('https://api.github.com/users/BolajiOlajide');
 
   const response = await resp.json()
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,23 +1,46 @@
-const { addNumbers } = require('.');
+const { addNumbers, subtractNumbers, generateId, makeAjaxCall } = require(".");
 
-describe('Test Suite', () => {
-  describe('addNumbers', () => {
-    test('adding 2 and 2 results to 4', () => {
+describe("Test Suite", () => {
+  describe("addNumbers", () => {
+    test("adding 2 and 2 results to 4", () => {
       const result = addNumbers(2, 2);
       expect(result).toEqual(4);
     });
   });
 
-  describe('subtractNumbers', () => {
-
+  describe("subtractNumbers", () => {
+    test("subtract two numbers ", () => {
+      const result = subtractNumbers(2, 10);
+      expect(result).toBe(-8);
+    });
   });
 
-  describe('makeAjaxCall', () => {
-    // remember to mock `node-fetch` with `jest.mock`
-    // an example can be found here: https://stackoverflow.com/questions/53484219/how-can-i-mock-fetch-function-in-node-js-by-jest
+  describe("makeAjaxCall", () => {
+    const fetch = require("node-fetch");
+    jest.mock("node-fetch", () => jest.fn());
+    it("checks fetch mock test", async () => {
+      const response = await Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => {
+          return { data: "the quick brown fox jumps over the lazy dog" };
+        },
+      });
+      fetch.mockImplementation(() => response);
+      makeAjaxCall()
+        .then(function (data) {
+          console.log("got data", data);
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+    });
   });
 
-  describe('generateId', () => {
-
+  describe("generateId", () => {
+    test("it prefixes 'random-' to the argument passed", () => {
+      const result = generateId("user001");
+      expect(result).toBe("random-user001");
+    });
   });
 });
